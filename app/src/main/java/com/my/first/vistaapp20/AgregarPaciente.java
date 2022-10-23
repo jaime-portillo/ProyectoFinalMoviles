e19ede431c007ce23bdc48f70fac33b3410bf94b
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-22 22:06:27 -0600

    activitys Add Paciente y Add optometrita

---
+++ app/src/main/java/com/my/first/vistaapp20/AgregarPaciente.java
@@ -0,0 +1,14 @@
+package com.my.first.vistaapp20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_paciente);
+    }
+}
