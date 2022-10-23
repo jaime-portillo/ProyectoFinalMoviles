2cab55a06eeda7948e5f4ba24ab1d9553ddebeb8
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-23 00:00:54 -0600

    menu add cotizacion and add productos

--- app/src/main/java/com/my/first/vistaapp20/Inicio.java
+++ app/src/main/java/com/my/first/vistaapp20/Inicio.java
@@ -1,8 +1,16 @@
 package com.my.first.vistaapp20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class Inicio extends AppCompatActivity {
 
@@ -11,4 +19,54 @@ public class Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(this, "Bienvendido a Vista Movil App", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_optometrista_add:
+                Intent intentOpt = new Intent(Inicio.this, AgregarOptometrista.class);
+                startActivity(intentOpt);
+                finish();
+                return true;
+            case R.id.nav_asesor_add:
+                Intent intentAsesor = new Intent(Inicio.this, AgregarAsesor.class);
+                startActivity(intentAsesor);
+                finish();
+                return true;
+            case R.id.nav_paciente_add:
+                Intent intentPaciente = new Intent(Inicio.this, AgregarPaciente.class);
+                startActivity(intentPaciente);
+                finish();
+                return true;
+            case R.id.nav_cita_add:
+                Intent intentCita = new Intent(Inicio.this, AgregarCita.class);
+                startActivity(intentCita);
+                finish();
+                return true;
+            case R.id.nav_cotizacion_add:
+                Intent intentCot = new Intent(Inicio.this, AgregarCotizacion.class);
+                startActivity(intentCot);
+                finish();
+                return true;
+            case R.id.nav_producto_add:
+                Intent intentPro = new Intent(Inicio.this, AgregarProducto.class);
+                startActivity(intentPro);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    }
