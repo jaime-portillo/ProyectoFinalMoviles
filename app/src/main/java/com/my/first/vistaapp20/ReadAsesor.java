144d05d617cc20ab60dc4c016b7f7d82658b74f9
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-11-06 22:17:37 -0600

    nombres de archivos

---
+++ app/src/main/java/com/my/first/vistaapp20/ReadAsesor.java
@@ -0,0 +1,14 @@
+package com.my.first.vistaapp20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadAsesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_asesor);
+    }
+}
---
+++ app/src/main/java/com/my/first/vistaapp20/ReadCita.java
@@ -0,0 +1,14 @@
+package com.my.first.vistaapp20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadCita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_cita);
+    }
+}
