7022ede1184708f351ab3460d0c540f2b8a6b0c1
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-11-07 13:39:34 -0600

    nombres de archivos

--- app/src/main/java/com/my/first/vistaapp20/AgregarCotizacion.java
+++ app/src/main/java/com/my/first/vistaapp20/AgregarCotizacion.java
@@ -7,10 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AgregarCotizacion extends AppCompatActivity {
-
+private EditText nombre, desAro, PrecioAro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
