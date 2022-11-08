994fb6917669da46f0d1c90d86eed6d8948ec1b9
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-11-08 01:10:40 -0600

    AgregarOptometrista a base

--- app/src/main/java/com/my/first/vistaapp20/AgregarOptometrista.java
+++ app/src/main/java/com/my/first/vistaapp20/AgregarOptometrista.java
@@ -1,20 +1,83 @@
 package com.my.first.vistaapp20;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class AgregarOptometrista extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgregarOptometrista extends AppCompatActivity {
+EditText txtnombre,txtfechaNacimiento,txtEdad,txtjvpm,txtDui,txtTelefono,txtDireccion;
+Button btnGuardar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_optometrista);
+
+        txtnombre=(EditText)findViewById(R.id.AOnombre);
+        txtfechaNacimiento=(EditText)findViewById(R.id.AOfecha);
+        txtEdad=(EditText)findViewById(R.id.AOedad);
+        txtjvpm=(EditText)findViewById(R.id.AOjvpm);
+        txtDui=(EditText)findViewById(R.id.AOdui);
+        txtTelefono=(EditText)findViewById(R.id.AOtelefono);
+        txtDireccion=(EditText)findViewById(R.id.AOdireccion);
+        btnGuardar=(Button)findViewById(R.id.AAguardar);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+ejecutarServicio("http://192.168.0.11:80/moviles/AddOptometrista.php");
+            }
+        });
+
+    }
+    public void ejecutarServicio(String URL){
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Operacion Exitosa", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String>parametros=new HashMap<String,String>();
+                parametros.put("Nombre_opt",txtnombre.getText().toString());
+                parametros.put("Fecha_nacimiento",txtfechaNacimiento.getText().toString());
+                parametros.put("Edad",txtEdad.getText().toString());
+                parametros.put("JVPM",txtjvpm.getText().toString());
+                parametros.put("Direccion",txtDireccion.getText().toString());
+                parametros.put("telefono",txtTelefono.getText().toString());
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
